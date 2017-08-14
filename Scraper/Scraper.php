f8faddc45c999a39a6ceedd0cc262b2d254b4b9e
Jeremy Knab <jmknab@cattco.org>
2017-08-14 11:52:44 -0400

    Attempt

--- Scraper/Scraper.php
+++ Scraper/Scraper.php
@@ -8,12 +8,14 @@
 namespace Cattaraugus;
 
 use Goutte\Client;
+use Symfony\Component\BrowserKit\Response;
+use Guzzle\Http\Message\Response as GuzzleResponse;
 
 // see https://stackoverflow.com/questions/21974796/access-guzzle-response-from-goutte
 class Scraper extends \Goutte\Client
 {
   protected $guzzleResponse;
-  protected function createResponse(\Guzzle\Http\Message\Response $response)
+  protected function createResponse(GuzzleResponse $response)
   {
     // \Guzzle\Http\Message\Response $response
     $this->guzzleResponse = $response;
