552f4f8116ab2b30603af5aa0301ce21f8cbcd6c
Jeremy Knab <jmknab@cattco.org>
2017-08-14 10:48:01 -0400

    Scraper class added

---
+++ Scraper/Scraper.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the web scraping package.
+ *
+ * Jeremy Knab <jknab@greenskunk.com>
+ *
+ */
+namespace Cattaraugus;
+
+use Goutte\Client;
+
+// see https://stackoverflow.com/questions/21974796/access-guzzle-response-from-goutte
+class Scraper extends \Goutte\Client
+{
+  protected $guzzleResponse;
+  protected function createResponse(ResponseInterface $response)
+  {
+    // \Guzzle\Http\Message\Response $response
+    $this->guzzleResponse = $response;
+    // return new Response((string) $response->getBody(), $response->getStatusCode(), $response->getHeaders());
+    return parent::createResponse($response);
+  }
+
+  /**
+   * @return \Guzzle\Http\Message\Response
+   */
+  public function getGuzzleResponse()
+  {
+    return $this->guzzleResponse;
+  }
+}
